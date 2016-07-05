fc8ed83a226d3d7f6c769d7d114e87cdeaad6ec8
nocotan <noconoco.lib@gmail.com>
2016-07-05 10:34:06 +0900

    update template

--- .vim/template/template.cpp
+++ .vim/template/template.cpp
@@ -1,39 +1,59 @@
+#define _GRIBCXX_DEBUG
+#include <algorithm>
+#include <cctype>
+#include <climits>
+#include <cmath>
+#include <complex>
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
-#include <cmath>
 #include <ctime>
-#include <cctype>
-#include <climits>
-#include <iostream>
-#include <string>
-#include <vector>
-#include <map>
-#include <list>
-#include <queue>
 #include <deque>
-#include <algorithm>
-#include <numeric>
-#include <utility>
-#include <complex>
-#include <memory>
 #include <functional>
+#include <iostream>
+#include <list>
+#include <map>
+#include <memory>
+#include <numeric>
+#include <queue>
+#include <string>
+#include <utility>
+#include <vector>
 using namespace std;
 
-#define ALL(g) (g).begin(), (g).end()
-#define REP(i, x, n) for(auto i=x; i<n; i++)
-#define rep(i, n) REP(i, 0, n)
-#define RET(i, x, n) for(auto i=x; i<=n; i++)
-#define ret(i, n) REP(i, 0, n)
-#define F(i, j, k) fill(i[0], i[0]+j*j, k)
-#define P(p) cout << (p) << endl;
-#define EXIST(s, e) ((s).find(e)!=(s).end())
-#define INF 1<<25
-#define pb push_back
+// 基本テンプレート
+#define REP(i,n) for(int i=0; i<(int)n; ++i)
+#define FOR(i,c) for(__typeof((c).begin())!=(c).begin(); i!=(c).end(); ++i)
+#define ALL(c) (c).begin(), c.end()
 
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef vector<double> vd;
 typedef vector<int> vi;
 typedef vector<long long> vl;
-typedef vector<double> vd;
-typedef pair<int, int> pii;
 typedef vector<long, long> pil;
-typedef long long ll;
+
+// グラフテンプレート
+typedef int Weight;
+
+struct Edge {
+  int src, dst;
+  Weight weight;
+  Edge(int src, int dst, Weight weight) :
+    src(src), dst(dst), weight(weight) {}
+};
+
+bool operator < (const Edge &e, const Edge &f) {
+  return e.weight != f.weight ? e.weight > f.weight :
+    e.src != f.src ? e.src < f.src : e.dst < f.dst;
+}
+
+typedef vector<Edge> Edges;
+typedef vector<Edges> Graph;
+typedef vector<Weight> Array;
+typedef vector<Array> Matrix;
+
+
+int main() {
+  return 0;
+}
