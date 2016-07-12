c20e7bb9a4c8273429ef4bdb4cc540648efbab02
nocotan <noconoco.lib@gmail.com>
2016-07-12 10:03:23 +0900

    template

--- .template/template.cpp
+++ .template/template.cpp
@@ -21,11 +21,12 @@
 #include <vector>
 using namespace std;
 
+
 // 基本テンプレート
 #define rep(i,n) for(int i=0; i<(int)n; ++i)
 #define REP(i,x,n) for(int i=x; i<(int)n; ++i)
 #define FOR(i,c) for(__typeof((c).begin())!=(c).begin(); i!=(c).end(); ++i)
-#define ALL(c) (c).begin(), c.end()
+#define ALL(c) (c).begin(), (c).end()
 
 typedef long long ll;
 typedef pair<int, int> pii;
@@ -33,6 +34,26 @@ typedef vector<double> vd;
 typedef vector<int> vi;
 typedef vector<long long> vl;
 typedef vector<long, long> pil;
+
+
+// Effective std
+template<typename C, typename T>
+constexpr int count(C& c, T t) { return count(ALL(c), t); }
+
+template<typename C, typename F>
+constexpr int count_if(C& c, F f) { return count_if(ALL(c), f); }
+
+template<typename C, typename T, typename U>
+constexpr void replace(C& c, T t, U u) { replace(ALL(c), t, u); }
+
+template<typename C, typename F, typename U>
+constexpr void replace_if(C& c, F f, U u) { (ALL(c), f, u); }
+
+template<typename C>
+constexpr void sort(C& c) { sort(ALL(c)); }
+
+template<typename C, typename Pred>
+constexpr void sort(C& c, Pred p) { sort(ALL(c), p); }
 
 
 // グラフテンプレート
