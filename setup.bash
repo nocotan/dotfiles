05ebbf5bdc49a0a0b10d2cffef140d64df1bfa2a
mkimura <mkimura@ridge-i.com>
2018-08-09 16:51:59 +0900

    update setup

--- setup.bash
+++ setup.bash
@@ -4,3 +4,10 @@ echo 'install neobundle...'
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh ./install.sh
 rm -rf install.sh
+
+echo 'install pyenva'
+git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
+echo 'eval "$(pyenv init -)"' >> ~/.bashrc
+source ~/.bashrc
